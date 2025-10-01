580a61b7de3d374fbdf872beb85492c76fa00636
Federico Diaz Bobillo <federicodiazbobillo@gmail.com>
2025-10-01 14:55:58 -0300

    .

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("cargando...");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/hello")   // 👈 apunta directo al backend
+      .then(res => res.json())
+      .then(data => setMsg(data.message))
+      .catch(err => setMsg("❌ Error: " + err.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Test React + API</h1>
+      <p>Respuesta: {msg}</p>
+    </div>
+  );
+}
+
+export default App;
