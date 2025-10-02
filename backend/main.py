4084394e7b4ca3b642f38b028f60868cb403b8a0
Federico Diaz Bobillo <federicodiazbobillo@gmail.com>
2025-10-02 13:43:30 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from api import bp as api_bp
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
-# registrar las rutas del blueprint
-app.register_blueprint(api_bp)
+    app.register_blueprint(api_bp)
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
