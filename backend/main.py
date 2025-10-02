25ebb623e54e1ebbfddf5433b508a2d2f0012e34
Federico Diaz Bobillo <federicodiazbobillo@gmail.com>
2025-10-02 14:52:16 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ from api import bp as api_bp
 def create_app():
     app = Flask(__name__)
     CORS(app)
-
+    # Permitir rutas con y sin barra final
+    app.url_map.strict_slashes = False
     app.register_blueprint(api_bp)
 
     return app
