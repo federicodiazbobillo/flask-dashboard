9791b82810512506678c54f75b63ad755ba31af3
Federico Diaz Bobillo <federicodiazbobillo@gmail.com>
2025-10-03 12:43:01 -0300

    .

--- backend/api/serverInfo/__init__.py
+++ backend/api/serverInfo/__init__.py
@@ -1,8 +1,10 @@
-from .serverInfo.cpu_info import cpu_bp
-from .serverInfo.memory_info import memory_bp
-from .serverInfo.storage_info import storage_bp
-from .serverInfo.gpu_info import gpu_bp
-from .serverInfo.net_info import net_bp
+from .cpu_info import cpu_bp
+from .memory_info import memory_bp
+from .storage_info import storage_bp
+from .gpu_info import gpu_bp
+from .net_info import net_bp
+
+bp_modules = [cpu_bp, memory_bp, storage_bp, gpu_bp, net_bp]
 
 def register_blueprints(app):
     app.register_blueprint(cpu_bp)
